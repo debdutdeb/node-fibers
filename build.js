429858dc23258d882490bd0b452c389f1585c295
Debdut Chakraborty <debdutdeb@outlook.com>
2023-11-16 18:26:01 +0530

    add node-gyp as a dependency

--- build.js
+++ build.js
@@ -27,17 +27,17 @@ var args = process.argv.slice(2).filter(function(arg) {
 if (!debug) {
 	args.push('--release');
 }
-if (!{ia32: true, x64: true, arm: true, arm64: true, ppc: true, ppc64: true, s390: true, s390x: true}.hasOwnProperty(arch)) {
-	console.error('Unsupported (?) architecture: `'+ arch+ '`');
+if (!{ ia32: true, x64: true, arm: true, arm64: true, ppc: true, ppc64: true, s390: true, s390x: true }.hasOwnProperty(arch)) {
+	console.error('Unsupported (?) architecture: `' + arch + '`');
 	process.exit(1);
 }
 
 // Test for pre-built library
-var modPath = platform+ '-'+ arch+ '-'+ process.versions.modules+ ((platform === 'linux') ? '-'+ detectLibc.family : '');
+var modPath = platform + '-' + arch + '-' + process.versions.modules + ((platform === 'linux') ? '-' + detectLibc.family : '');
 if (!force) {
 	try {
 		fs.statSync(path.join(__dirname, 'bin', modPath, 'fibers.node'));
-		console.log('`'+ modPath+ '` exists; testing');
+		console.log('`' + modPath + '` exists; testing');
 		cp.execFile(process.execPath, ['quick-test'], function(err, stdout, stderr) {
 			if (err || stdout !== 'pass' || stderr) {
 				console.log('Problem with the binary; manual build incoming');
@@ -57,36 +57,37 @@ if (!force) {
 // Build it
 function build() {
 	if (process.versions.electron) {
-		args.push('--target='+ process.versions.electron,  '--dist-url=https://atom.io/download/atom-shell');
+		args.push('--target=' + process.versions.electron, '--dist-url=https://atom.io/download/atom-shell');
 	}
+	verifyNodeGyp();
 	cp.spawn(
 		process.platform === 'win32' ? 'node-gyp.cmd' : 'node-gyp',
 		['rebuild'].concat(args),
-		{stdio: [process.stdin, process.stdout, process.stderr]})
-	.on('exit', function(err) {
-		if (err) {
+		{ stdio: [process.stdin, process.stdout, process.stderr] })
+		.on('exit', function(err) {
+			if (err) {
+				console.error(
+					'node-gyp exited with code: ' + err + '\n' +
+					'Please make sure you are using a supported platform and node version. If you\n' +
+					'would like to compile fibers on this machine please make sure you have setup your\n' +
+					'build environment--\n' +
+					'Windows + OS X instructions here: https://github.com/nodejs/node-gyp\n' +
+					'Ubuntu users please run: `sudo apt-get install g++ build-essential`\n' +
+					'RHEL users please run: `yum install gcc-c++` and `yum groupinstall \'Development Tools\'` \n' +
+					'Alpine users please run: `sudo apk add python make g++`'
+				);
+				return process.exit(err);
+			}
+			afterBuild();
+		})
+		.on('error', function(err) {
 			console.error(
-				'node-gyp exited with code: '+ err+ '\n'+
-				'Please make sure you are using a supported platform and node version. If you\n'+
-				'would like to compile fibers on this machine please make sure you have setup your\n'+
-				'build environment--\n'+
-				'Windows + OS X instructions here: https://github.com/nodejs/node-gyp\n'+
-				'Ubuntu users please run: `sudo apt-get install g++ build-essential`\n'+
-				'RHEL users please run: `yum install gcc-c++` and `yum groupinstall \'Development Tools\'` \n'+
-				'Alpine users please run: `sudo apk add python make g++`'
+				'node-gyp not found! Please ensure node-gyp is in your PATH--\n' +
+				'Try running: `sudo npm install -g node-gyp`'
 			);
-			return process.exit(err);
-		}
-		afterBuild();
-	})
-	.on('error', function(err) {
-		console.error(
-			'node-gyp not found! Please ensure node-gyp is in your PATH--\n'+
-			'Try running: `sudo npm install -g node-gyp`'
-		);
-		console.log(err.message);
-		process.exit(1);
-	});
+			console.log(err.message);
+			process.exit(1);
+		});
 }
 
 // Move it to expected location
@@ -96,7 +97,7 @@ function afterBuild() {
 
 	try {
 		fs.mkdirSync(path.join(__dirname, 'bin', modPath));
-	} catch (ex) {}
+	} catch (ex) { }
 
 	try {
 		fs.statSync(targetPath);
@@ -105,10 +106,20 @@ function afterBuild() {
 		process.exit(1);
 	}
 	fs.renameSync(targetPath, installPath);
-	console.log('Installed in `'+ installPath+ '`');
+	console.log('Installed in `' + installPath + '`');
 	if (process.versions.electron) {
 		process.nextTick(function() {
 			require('electron').app.quit();
 		});
 	}
+}
+
+function verifyNodeGyp() {
+	cp.spawnSync('npm', ['install', 'node-gyp@9.4.1', '-g'], {
+		stdio: [process.stdin, process.stdout, process.stderr]
+	}).on('exit', (err) => {
+		if (err) {
+			console.error(err);
+		}
+	});;
 }
